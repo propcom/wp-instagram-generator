2148d4cbe43f4afb55f56283a69db53037be12d5
Simon Shahriveri <si@hi-im-si.com>
2019-04-17 15:50:23 +0100

    Debug

--- class-data-handling.php
+++ class-data-handling.php
@@ -22,7 +22,7 @@ class InstagramFetch {
 	public function __construct( $count = 30 ) {
 
 		// this will set the option to be the propeller manager social page
-		$option = get_option('prop_social');
+		$option = get_option('prop_social') || get_field('instagram_token', 'option');
 		// set the access token to be token field of the option
 		$this->access_token = $option['instagram_token'];
 
